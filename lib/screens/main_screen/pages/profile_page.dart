ade8d68032711cb3228a91baaa4316d04d3e7273
Mian Mudarrib <mmudarrib2807@gmail.com>
2021-12-19 19:38:03 +0500

    dont know what i just did

--- lib/screens/main_screen/pages/profile_page.dart
+++ lib/screens/main_screen/pages/profile_page.dart
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import 'package:digiloger/database/digilog_api.dart';
 import 'package:digiloger/models/digilog.dart';
 import 'package:extended_image/extended_image.dart';
-=======
->>>>>>> 0198e2264cb9b867f15183a1c1bea21c4c9dbc6a
 import 'package:flutter/material.dart';
 import '../../../services/user_local_data.dart';
 import '../../../utilities/custom_image.dart';
@@ -48,53 +45,6 @@ class _ProfilePageState extends State<ProfilePage> {
           const SizedBox(height: 20),
           const Divider(),
           Expanded(
-<<<<<<< HEAD
-            child: FutureBuilder<List<Digilog>>(
-                future: getdigilogs(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    List<Digilog> _digiLog = snapshot.data!;
-                    if (_digiLog.isNotEmpty) {
-                      return GridView.builder(
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3,
-                            crossAxisSpacing: 2,
-                            mainAxisSpacing: 2,
-                          ),
-                          shrinkWrap: true,
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return /*isUrl
-                                ? ExtendedImage.network(
-                                    _digiLog[index].experiences[0].mediaUrl,
-                                    fit: BoxFit.fill,
-                                  )
-                                : ExtendedImage.file(
-                                    File(_digiLog[index]
-                                        .experiences[0]
-                                        .mediaUrl),
-                                    fit: BoxFit.fill,
-                                  );*/
-                                tile(_digiLog[index]);
-                          });
-                    } else {
-                      return Text(
-                        "No Posted Digilogs",
-                        style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 22),
-                      );
-                    }
-                  } else {
-                    return Text(
-                      "No Posted Digilogs",
-                      style: TextStyle(
-                          color: Theme.of(context).primaryColor, fontSize: 22),
-                    );
-                  }
-                }),
-=======
             child: GridViewOfPosts(
               posts: <String>[
                 CustomImages.domeURL,
@@ -107,17 +57,9 @@ class _ProfilePageState extends State<ProfilePage> {
                 CustomImages.domeURL,
               ],
             ),
->>>>>>> 0198e2264cb9b867f15183a1c1bea21c4c9dbc6a
           )
         ],
       ),
-    );
-  }
-
-  ExtendedImage tile(Digilog digiLog) {
-    return ExtendedImage.network(
-      digiLog.experiences.first.mediaUrl,
-      fit: BoxFit.fill,
     );
   }
 
